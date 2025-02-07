484955dc516276570ff312ad7da08695222b6bc1
JJ Garzella <jjgarzella@gmail.com>
2025-02-06 21:46:21 -0800

    Add cr strings
    cr coming from the controlledreduction repo that isn't
    fully wrapped yet

--- src/ToricControlledReduction.jl
+++ src/ToricControlledReduction.jl
@@ -20,45 +20,77 @@ function projective_space_fan_string(n)
 
   projstring
 end#function
+"""
+outputs the exponent vector of f in the form used
+by controlled reduction scripts
 
-function tcr_string(p,poly,name)
-  tcrstring = name * ":"
+Note: use trim=true for ToricControlledReduction,
+use trim=false for controlledreduction
+"""
+function exponent_vector_string(f;trim=true)
+  ev_string = ""
+  ev_string *= "["
 
-  n = length(gens(parent(poly)))
-
-  # put the exponent vectors here
-  tcrstring = tcrstring * "["
-
-  for exp_vec in exponent_vectors(poly)
-    tcrstring = tcrstring * "[" * join(exp_vec[1:end-1], " ") * "]"
+  for exp_vec in exponent_vectors(f)
+    trimfactor = trim ? 1 : 0
+    ev_string *= "[" * join(exp_vec[1:end-trimfactor], " ") * "]"
   end
 
-  tcrstring = tcrstring * "]:"
+  ev_string *= "]"
+
+  ev_string
+end
+
+coeffs_string(f) = "[" * join(coefficients(f)," ") * "]"
+
+function tcr_string(p,f,name)
+  tcrstring = name * ":"
+
+  n = length(gens(parent(f)))
+
+  # put the exponent vectors here
+  
+  tcrstring *= exponent_vector_string(f)
+
+  tcrstring *= ":"
 
   # put the coefficients here
   
-  tcrstring = tcrstring * "[" * join(coefficients(poly)," ") * "]:"
+  tcrstring *= coeffs_string(f)
+  tcrstring *= ":"
 
   # The fan for projective space
 
-  tcrstring = tcrstring * projective_space_fan_string(n) * ":"
+  tcrstring *=  projective_space_fan_string(n) * ":"
 
   # the degree here
 
   degarr = zeros(Int,n)
-  deg = sum(exponent_vectors(poly)[1]) # we assume homogeneous
+  deg = sum(exponent_vectors(f)[1]) # we assume homogeneous
   degarr[1] = deg
   
-  tcrstring = tcrstring * "[" * join(degarr," ") * "]"
+  tcrstring *= "[" * join(degarr," ") * "]"
 
   # the prime number
 
-  tcrstring = tcrstring * ":" * string(p)
+  tcrstring *= ":" * string(p)
 
   # println(tcrstring)
 
   tcrstring
 end#function
+
+function cr_string(p,f)
+  crstring = "$p\n"
+  crstring *= exponent_vector_string(f,trim=false) * "\n"
+  crstring *= coeffs_string(f)
+
+  crstring
+end
+
+#
+#  # similar to tcr_string
+#end
 
 function output_tcr_file(ps,polys,names,outputfilename)
   open(outputfilename,"w") do outfile
@@ -78,8 +110,8 @@ end
 """
 assumes julia is running from the project directory
 """
-function zeta_function_tcr(p,f)
-  zeta_function_tcr([p],[f])
+function zeta_function_tcr(p,polynomial)
+  zeta_function_tcr([p],[polynomial])
 end
 
 """
@@ -93,7 +125,7 @@ function zeta_function_tcr(ps::Array,fs::Array)
   names = fill("",l)
   for i in 1:l
     d = degree(f,1) # assumes homogeneous
-    n = length(gens(parent(f)))
+    n = length(gens(parent(fs[i])))
     names[i] = "example$(i)_dim$(n-1)_degree$(d)_" * unique_id
   end
   filename = "data/examples_" * unique_id
